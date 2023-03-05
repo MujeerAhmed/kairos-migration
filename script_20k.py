8200916378e09b3ac6ed439a18bdd90e0c7f3f69
Mujeer Ahmed <117839246+MujeerAhmed@users.noreply.github.com>
2023-03-05 14:32:18 +0530

    Optimized script_20k.py

--- script_20k.py
+++ script_20k.py
@@ -2,6 +2,7 @@ import pyodbc
 import time
 from datetime import datetime
 import requests
+
 session  = requests.Session()
 
 # Connection string for SQL Server
@@ -13,54 +14,43 @@ conn = pyodbc.connect(conn_string)
 # Create a cursor
 cursor = conn.cursor()
 
+# Execute a SQL query
+query = 'SELECT * FROM [panndetekt].[dbo].[DB2] WHERE TagName = \'10HCC01CF001\';'
+cursor.execute(query)
+
+# Fetch the results
+results = cursor.fetchall()
+
+# Convert timestamp to epoch format
+epoch_results = []
+for row in results:
+    dt = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f0')  # Convert string to datetime object
+    epoch_time = int(time.mktime(dt.timetuple()))
+    epoch_time = epoch_time*1000
+    epoch_row = [epoch_time] + list(row[1:])
+    epoch_results.append(epoch_row)
+
 # Define the chunk size
 chunk_size = 20000
 
-# Execute a SQL query to get the total number of rows for the given TagName
-count_query = 'SELECT COUNT(*) FROM [panndetekt].[dbo].[DB2] WHERE TagName = \'1HJF20CF001C\';'
-cursor.execute(count_query)
-total_rows = cursor.fetchone()[0]
-
-# Loop through the data in chunks of 20,000 rows
-for i in range(0, total_rows, chunk_size):
-    # Execute a SQL query to fetch the next chunk of data
-    query = f'SELECT * FROM [panndetekt].[dbo].[DB2] WHERE TagName = \'1HJF20CF001C\' ORDER BY Timestamp OFFSET {i} ROWS FETCH NEXT {chunk_size} ROWS ONLY;'
-    cursor.execute(query)
-    results = cursor.fetchall()
-
-    # Convert timestamp to epoch format
-    epoch_results = []
-    for row in results:
-        dt = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f0')  # Convert string to datetime object
-        epoch_time = int(time.mktime(dt.timetuple()))
-        epoch_time = epoch_time*1000
-        epoch_row = [epoch_time] + list(row[1:])
-        epoch_results.append(epoch_row)
-
-    post_list = [[sublist[0], sublist[2]] for sublist in epoch_results]
-
+# Post data in chunks
+for i in range(0, len(epoch_results), chunk_size):
+    post_list = [[sublist[0], sublist[2]] for sublist in epoch_results[i:i+chunk_size]]
     url = "http://13.68.199.3/kairosapi/api/v1/datapoints"
-    col = "UMEA_1HJF20CF001C"
-    def post(url,store_vals,col):
-        body = [{
-                "name": str(col),
-                # "datapoints": [toDatapointsFormat(x) for x in store_vals],
-                "datapoints": store_vals,
-                "tags": {
-                "type": "eqp_state"
-                }}]
-        # print(body)
-    #    print(url, col)
-        res = requests.post(url=url,json = body,stream=True)
-        print(res.content)
-        print(res.status_code)
-        if res.status_code == 204:
-           
-            print ('Data Posted Successfully!')
-        else:
-            print("Post data fail")
-
-    post(url, post_list, col)
+    col = "UMEA_10HCC01CF001"
+    body = [{
+            "name": str(col),
+            "datapoints": post_list,
+            "tags": {
+            "type": "eqp_state"
+            }}]
+    res = requests.post(url=url, json=body, stream=True)
+    print(res.content)
+    print(res.status_code)
+    if res.status_code == 204:
+        print('Data Posted Successfully!')
+    else:
+        print("Post data fail")
 
 cursor.close()
 conn.close()
